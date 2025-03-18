1d85cec7bd5e7061ac0ce9ed379dd1c3cf594eea
Jordan Lucas <jordanrlucas@gmail.com>
2025-03-18 18:21:53 +0000

    chapter 1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import React, { Component } from 'react';
+import Welcome from './components/Welcome';
+import Support from './components/Support';
+import Listcast from './components/ListCast';
+import ListCast from './components/ListCast';
+
 function App() {
+  const name = 'StarGazers'
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="StarGazers Group" />
+          <Welcome name="StarGazers" />
+          <p>Members of an <b>intergalactic alliance</b><br />
+            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <ListCast />
+          <Support />
+        </hgroup>
+      </article>
+    </div>
   )
 }
 export default App
